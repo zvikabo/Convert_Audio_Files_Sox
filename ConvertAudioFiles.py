913fe58d6d8062fe0052958090390fda2ac9f195
Zvikabo <Zvikabo@gmail.com>
2022-03-11 20:03:40 +0200

    First commit : This script convert Audio format using SOX

---
+++ ConvertAudioFiles.py
@@ -0,0 +1,43 @@
+"""
+This script convert music files format using sox (Free music format converter)
+by using subprocess with the relevant parameters:
+Channels =1 means mono
+rate bit = 8000 Mhz
+encoding : A-Laq
+using soxi.exe ( Actually a copy od SOX.exe and rename it to soxi.exe return ths file properties
+"""
+
+import subprocess
+import glob
+import logging
+
+sox_install_path = 'C:\\Program Files (x86)\\sox-14-4-2'
+source_path = 'C:\\PythonTemp\\AudioSox\\Src'
+source_file_suffix = 'WAV'
+destination_path = 'C:\\PythonTemp\\AudioSox\\Dst'
+convert_file_prefix = 'Heb_'
+convert_file_suffix = 'wav'
+channels = '1'
+rate = '8000'
+encoding = 'a-law'
+
+Log_path = 'C:\\PythonTemp\\AudioSox\\Logs'
+logger = logging.getLogger('ConvertAudioFiles     ')
+hdlr = logging.FileHandler(Log_path + '\\' + 'ConvertAudioFiles.log')
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
+logger.setLevel(logging.DEBUG)
+
+for f in glob.glob(source_path + '\\' + '*.' + source_file_suffix):
+    p = subprocess.Popen([sox_install_path + '\\sox.exe', f, '-c', channels, '-r', rate, '-e', encoding,
+                          destination_path + '\\' + convert_file_prefix + (f.split('.')[-2]).split('\\')[
+                              -1] + '.' + convert_file_suffix], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = p.communicate()
+    if p.returncode == 0:
+        subprocess.call([sox_install_path + '\\soxi.exe',
+                         destination_path + '\\' + convert_file_prefix + (f.split('.')[-2]).split('\\')[
+                             -1] + '.' + convert_file_suffix])
+        logger.debug(f'{f} has been converted successfully ')
+    else:
+        logger.debug(f'Fail to convert {f}')
---
+++ write2file.py
@@ -0,0 +1,22 @@
+"""
+This script open file and append string to the file and than close the file
+"""
+from __future__ import unicode_literals
+
+
+def main():
+
+    f='C:\\PythonTemp\write2file\write2file.txt'
+    g= open(f,'a')
+    for i in range(10):
+        g.write('{} ddhnfmf,mdfdf,mdsf,mn,mfnmdfnmdnfmdn \n ' .format(i) )
+        g.flush()
+    g.close()
+
+if __name__=='__main__':
+    main()
+
+
+import paramiko
+www=paramiko.SSHClient.connect('aaa','bbb','ccc')
+www
